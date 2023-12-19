6e202d7c25b0ae05f8712e7d99544574f645d11d
Bazga2k1 <filippo.bubic@gmail.com>
2023-12-19 16:58:20 +0100

    Specifikacija backenda
    - instaliran MongoDB
    - konstruiran kostur backenda

---
+++ index.js
@@ -0,0 +1,106 @@
+import express from "express";
+import cors from "cors";
+import { AutoEncryptionLoggerLevel } from "mongodb";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Registracija
+app.post("/register", (req, res) => {
+    const { ime_firme, ime_prezime, oib, email, password } = req.body;
+  
+    if (users.some((user) => user.ime_firme === ime_firme && user.oib === oib)) { // Provjera postojanja registracije
+      return res.status(400).json({ error: "Ova firma je već registrirana!" });
+    }
+  
+    const newUser = {
+      id: users.length + 1,
+      ime_firme,
+      ime_prezime,
+      oib,
+      email,
+      password 
+    };
+  
+    users.push(newUser);
+  
+    res.status(201).json({ id: newUser.id, ime_firme: newUser.ime_firme, oib: newUser.oib });
+
+
+let cars = [];
+let rims = [];
+let engines = [];
+let interiors = [];
+let colors = [];
+
+// Inventar automobila
+app.get('/', (req, res) => {
+    res.json(cars);
+});
+
+app.post('/', (req, res) => { // Odabir automobila
+    let odabirAuto = req.body;
+    cars.push(odabirAuto);
+    res.json(odabirAuto);
+});
+
+// Inventar naplataka
+app.get('/konfiguracija1', (req, res) => {
+    res.json(rims);
+});
+
+app.post('/konfiguracija1', (req, res) => { // Odabir naplataka
+    let OdabirNap = req.body;
+    rims.push(OdabirNap);
+    res.json(OdabirNap);
+});
+
+// Inventar motora
+app.get('/konfiguracija2', (req, res) => {
+    res.json(engines);
+});
+
+app.post('/konfiguracija2', (req, res) => { // Odabir motora
+    let odabirMot = req.body;
+    engines.push(odabirMot);
+    res.json(odabirMot);
+});
+
+// Inventar interijera
+app.get('/konfiguracija3', (req, res) => {
+    res.json(interiors);
+});
+
+app.post('/konfiguracija3', (req, res) => { // Odabir interijera
+    let odabirInt = req.body;
+    interiors.push(odabirInt);
+    res.json(odabirInt);
+});
+
+// Inventar boja
+app.get('/konfiguracija4', (req, res) => {
+    res.json(colors);
+});
+
+app.post('/konfiguracija4', (req, res) => { // Odabir boja
+    let odabirBoj = req.body;
+    colors.push(odabirBoj);
+    res.json(odabirBoj);
+});
+
+// Total narudžbe
+app.post('/narudzba', (req, res) => {
+    let narudzba = req.body.narudzba;
+    let deliveryLocation = req.body.deliveryLocation;
+    //let total_price = calculateTotalPrice(narudzba);
+    res.json({narudzba: narudzba, deliveryLocation: deliveryLocation, total_price: total_price});
+});
+
+/* Funkcija za izračunavanje sveukupne cijene
+function calculateTotalPrice(order) {
+
+    return 0;
+}*/
+
+});
